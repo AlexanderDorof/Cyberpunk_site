32d83a4bad1a5d527f5b6d67de744590f42bffd5
Alexander <venussd96@gmail.com>
2024-01-03 04:38:09 +0300

    weapon added

--- cyberpunk/implants/views.py
+++ cyberpunk/implants/views.py
@@ -11,9 +11,38 @@ def gather_base_context():
                }
     return context
 
-def add_item_test():
-    item = Cyberware(category='katamaran', description='tatatatat', href='implants',photo='illustration/Menu_preview/Moto.jpg', Characteristic="BODY", is_available=True, capacity=10, price=10, cyberware_category=Cyberware_category.objects.get(pk=1))
-    item.save()
+def create_in_db():
+    import os
+    from bs4 import BeautifulSoup
+    from PIL import Image
+    import random
+
+
+    def save_items(image_file, iter_data, number_id):
+        title, description = next(iter_data)[:-1].split('|')
+        title = title.strip()
+        description = description.strip()
+        img = 'illustration/weapons/' + image_file
+        print(title, description, img)
+        item = Weapon(category=title, description=description, href=number_id,
+                         photo=img, weapon_category=Weapon_category.objects.get(pk=1), is_available=True,
+                         damage=random.randint(2, 20),
+                         load_capacity=random.randint(2, 30),
+                         price=random.randint(10, 100))
+        item.save()
+
+
+
+
+
+    all_img = os.listdir(r'D:\My projects\Python\TeachMeSkills\Django_Cyberpunk\cyberpunk\media\illustration\weapons')
+    with open(r'D:\My projects\Python\Learn_BeautifulSoup\cybweapons\title_description.txt') as file:
+        data = file.readlines()
+    iter_data = iter(data)
+    number_id=200
+    for image_file in all_img:
+        number_id += 1
+        save_items(image_file, iter_data, number_id)
 
 
 
@@ -30,13 +59,6 @@ def categories(request, category_path):
     dict_href = {'implants': Cyberware_category, 'trans': Auto_category, 'weapons': Weapon_category,
                  'clothes': Clothes_category, 'items': Items_category}
     chosen_category = dict_href[category_path]
-    # if '/' in category_path:
-    #     category_path = category_path.split('/')[1]
-    #     if category_path.isdigit():
-    #         return redirect('archive', item=category_path, permanent=True)
-    #     chosen_category = dict_href[category_path]
-    # else:
-    #     chosen_category = dict_href[category_path]
     cards = chosen_category.objects.all()
     context = {
         'title': 'CyberShop',
@@ -46,11 +68,16 @@ def categories(request, category_path):
     return render(request, 'implants/sidemenu.html', context=context)
 
 def categories_items(request, category_path, category_items):
+    if category_items.isdigit():
+        return redirect('archive', item=category_items, permanent=True)
     dict_href = {'implants': Cyberware_category, 'trans': Auto_category, 'weapons': Weapon_category,
                  'clothes': Clothes_category, 'items': Items_category}
     chosen_category = dict_href[category_path]
-    x = Cyberware_category.objects.get(href=f"{category_path}/{category_items}")
-    cards = Cyberware.objects.filter(cyberware_category=x)
+    x = chosen_category.objects.get(href=f"{category_path}/{category_items}")
+    if category_path == 'implants':
+        cards = Cyberware.objects.filter(cyberware_category=x)
+    else:
+        cards = Weapon.objects.filter(weapon_category=x)
     context = {
         'title': 'CyberShop',
         'cards': cards,
@@ -62,7 +89,10 @@ def categories_items(request, category_path, category_items):
 def archive(request, item):
     catalog = [item.href for item in Cyberware_category.objects.all()]
     dict_href = {}
-    item_to_display = Cyberware.objects.filter(href=item)[0]
+    if item < 200:
+        item_to_display = Cyberware.objects.filter(href=item)[0]
+    else:
+        item_to_display = Weapon.objects.filter(href=item)[0]
     if item in dict_href.keys():
         return redirect('home', permanent=True)
     context = {
